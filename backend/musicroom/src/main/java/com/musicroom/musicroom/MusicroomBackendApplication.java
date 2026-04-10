127b4ed8b6d88eb7889977ca378ad215a1183d7b
Oubeid Hamad <OubeidH@algoconsulting.net>
2026-04-10 20:30:51 +0100

    add table users and playlist

---
+++ backend/musicroom/src/main/java/com/musicroom/musicroom/MusicroomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musicroom.musicroom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicroomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicroomBackendApplication.class, args);
+	}
+
+}
